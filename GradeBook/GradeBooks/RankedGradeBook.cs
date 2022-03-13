8ee88933170bc1e45a8e6fa984ba9dcb40b03104
mszczechowicz <scela0718@gmail.com>
2022-03-13 22:50:17 +0100

    1.Update StartingUserInterface's CreateCommand method.
    2.Update StartingUserInterfaces's HelpCommand method.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -7,8 +7,6 @@ namespace GradeBook.GradeBooks
     public class RankedGradeBook : BaseGradeBook
     {
 
-        public GradeBookType Type { get; set; }
-
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
